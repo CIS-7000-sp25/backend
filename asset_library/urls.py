1f859ade91f363db795293391f215a46a23cdffe
mialana <96770662+mialana@users.noreply.github.com>
2025-04-28 14:13:21 -0400

    set explicit URL

--- asset_library/urls.py
+++ asset_library/urls.py
@@ -33,6 +33,7 @@ schema_view = get_schema_view(
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
+   url='https://backend-production-9a54.up.railway.app'
 )
 
 urlpatterns = [
