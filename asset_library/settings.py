fe7385003b73c3c79709dc06ead3a7dd8b4c3c28
mialana <96770662+mialana@users.noreply.github.com>
2025-04-28 14:09:51 -0400

    add test server

--- asset_library/settings.py
+++ asset_library/settings.py
@@ -39,6 +39,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://usd-asset-library.up.railway.app",
+    "https://backend-production-9a54.up.railway.app/",
     "http://localhost:8000"
 ]
 
