76f2925513d12bee189b0a4cd1ffc1721cab2adb
mialana <96770662+mialana@users.noreply.github.com>
2025-04-28 04:04:43 -0400

    Implement download by tag

--- library/views_download.py
+++ library/views_download.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
-from library.models import Asset, Commit, Sublayer
+from library.models import Asset, Commit, Sublayer, StatusTag
 from library.utils.s3_utils import S3Manager
 from library.utils.zipper import zip_files_from_memory
 
@@ -26,19 +26,9 @@ def download_asset_by_version(request, asset_name, version):
     zip_data = {}
     for sublayer in commit.sublayers.all():
         sublayer: Sublayer
-        _sublayer_filepath = sublayer.filepath.split("/", maxsplit=1)
         
-        s3_bucket = _sublayer_filepath[0]
-        s3_key = _sublayer_filepath[1]
-
-        try:
-            file_bytes = s3.download_s3_file(key=s3_key, bucket=s3_bucket)
-        except Exception as e:
-            raise SystemError(f"Error occured in downloading the file from S3: {e}")
-        
-        print(f"[DEBUG] Downloaded '{s3_key}' ({len(file_bytes)} bytes)")
-
-        truncated_key = s3_key[s3_key.find(asset_name):] # truncates key to beginning of first occurence of asset_name
+        # continue with new found sublayer of desired tag
+        truncated_key, file_bytes = _get_bytes_for_sublayer(s3, sublayer)
 
         path_in_zip = os.path.join(ZIP_PREFIX, truncated_key)
         
@@ -47,7 +37,7 @@ def download_asset_by_version(request, asset_name, version):
     zip_buffer = zip_files_from_memory(zip_data)
 
     response = StreamingHttpResponse(zip_buffer, content_type='application/zip')
-    response['Content-Disposition'] = f'attachment; filename="{asset_name}_{version}.zip"'
+    response['Content-Disposition'] = f'attachment; filename=f"{ZIP_PREFIX}.zip"'
     return response
 
 @swagger_auto_schema(
@@ -60,6 +50,58 @@ def download_asset_by_version(request, asset_name, version):
             type=openapi.TYPE_STRING,
         ),
     ], responses={200: openapi.Response(description="Binary file stream", content_type="application/zip")})
+
 @api_view(['GET'])
-def download_asset_by_tag(request, assetName, tag: str):
-    pass
+def download_asset_by_tag(request, asset_name, tag: str):
+    s3 = S3Manager()
+
+    asset = get_object_or_404(Asset, assetName=asset_name)
+    tagObj = get_object_or_404(StatusTag, statusTag=tag)
+    commit = Commit.objects.filter(asset=asset).order_by('-version')[0] # latest commit
+
+    ZIP_PREFIX=f"{asset_name}_{tag}"
+
+    zip_data = {}
+    for sl in commit.sublayers.all():
+        sublayer: Sublayer = sl
+        while True:
+            if sublayer.status == tagObj:
+                break
+            elif sublayer.prevVersion:
+                sublayer = sublayer.prevVersion
+            else: # did not find a sublayer with the desired tag
+                sublayer = sl # set to original sublayer in case our desired tag is "latest"
+                break
+
+        if sublayer == sl and tagObj.statusTag != "latest": 
+            continue # this sublayer does not have the desired tag in its history. don't include in zip
+
+        # continue with new found sublayer of desired tag
+        truncated_key, file_bytes = _get_bytes_for_sublayer(s3, sublayer)
+
+        path_in_zip = os.path.join(ZIP_PREFIX, truncated_key)
+        
+        zip_data[path_in_zip] = file_bytes
+
+    zip_buffer = zip_files_from_memory(zip_data)
+
+    response = StreamingHttpResponse(zip_buffer, content_type='application/zip')
+    response['Content-Disposition'] = f'attachment; filename=f"{ZIP_PREFIX}.zip"'
+    return response
+
+def _get_bytes_for_sublayer(s3: S3Manager, sublayer: Sublayer) -> tuple[str, bytes]: # returns: truncated s3 key, bytes of file
+    _sublayer_filepath = sublayer.filepath.split("/", maxsplit=1)
+        
+    s3_bucket = _sublayer_filepath[0]
+    s3_key = _sublayer_filepath[1]
+
+    try:
+        file_bytes = s3.download_s3_file(key=s3_key, bucket=s3_bucket)
+    except Exception as e:
+        raise SystemError(f"Error occured in downloading the file from S3: {e}")
+    
+    print(f"[DEBUG] Downloaded '{s3_key}' ({len(file_bytes)} bytes)")
+
+    truncated_key = s3_key[s3_key.find(sublayer.asset.assetName):] # truncates key to beginning of first occurence of asset_name
+
+    return truncated_key, file_bytes
