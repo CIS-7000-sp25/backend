1cd8d14cf7a8959063930e8312733f22057df3f2
jyguan18 <jyguan@seas.upenn.edu>
2025-04-28 23:00:10 -0400

    add isStrict for QC

--- library/views_upload.py
+++ library/views_upload.py
@@ -21,7 +21,7 @@ from library.usd_validation import (
     core, geometry, materials, references, structure
 )
 
-def verify_asset(extracted_file, file_name, tmp_dir, asset_name):
+def verify_asset(extracted_file, file_name, tmp_dir, asset_name, isStrict):
     try:
         # Write the in-memory file to a temporary file
         with tempfile.NamedTemporaryFile(suffix=".usd", delete=False) as tmp:
@@ -31,12 +31,16 @@ def verify_asset(extracted_file, file_name, tmp_dir, asset_name):
             tmp_filename = tmp.name
 
         stage = Usd.Stage.Open(tmp_filename)
-        core.check_usd_properties(stage, file_name)
-        geometry.check_usd_geometry(stage, file_name)
-        materials.check_usd_materials(stage, file_name)
-        references.check_usd_references(stage, Path(file_name), tmp_dir)
-        if Path(file_name).stem == Path(file_name).parents[0].name:
-            structure.check_usd_structure(stage, file_name, tmp_dir, asset_name)
+
+        if (isStrict):
+            core.check_usd_properties(stage, file_name)
+            geometry.check_usd_geometry(stage, file_name)
+            materials.check_usd_materials(stage, file_name)
+            references.check_usd_references(stage, Path(file_name), tmp_dir)
+            if Path(file_name).stem == Path(file_name).parents[0].name:
+                structure.check_usd_structure(file_name, tmp_dir, asset_name)
+        else:
+            structure.check_basic_structure(file_name, tmp_dir, asset_name)
 
         return (True, "No error")
     except AssertionError as ae:
@@ -46,7 +50,7 @@ def verify_asset(extracted_file, file_name, tmp_dir, asset_name):
         print(f"General Exception: {e}")
         return (False, f"Unexpected error: {e}")
 
-def validate_zip(request, asset_name):
+def validate_zip(request, asset_name, isStrict):
     # TO DO: Find a way to cache this result so upload will grab results from verify
     zip = request.FILES.get('file')
 
@@ -87,7 +91,7 @@ def validate_zip(request, asset_name):
 
             if file_info.filename.endswith('.usd') or file_info.filename.endswith('.usda'):
                 with open(temp_dir / file_info.filename, 'rb') as extracted_file:
-                    fileResult = verify_asset(extracted_file, file_info.filename, temp_dir, asset_name)
+                    fileResult = verify_asset(extracted_file, file_info.filename, temp_dir, asset_name, isStrict)
 
                     newStatus: bool = result[0] and fileResult[0]
                     newMessage: str = result[1]
@@ -115,7 +119,7 @@ def get_verify(request, asset_name):
         isStrict = serializer.validated_data.get("isStrict")
         print(f"QC isStrict = {isStrict}")
 
-        result, error_msg = validate_zip(request, asset_name)
+        result, error_msg = validate_zip(request, asset_name, isStrict)
         if result:
             return Response({'success': True, 'message': "Passed validation!"}, status=200) 
         else:
